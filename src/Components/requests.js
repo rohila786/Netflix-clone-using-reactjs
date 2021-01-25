b3c0401f679e9b6934b231d1016cb1af7f8d8a3d
Jaswant <rohilajaswant786@gmail.com>
2021-01-25 10:52:30 +0530

    1st commit

---
+++ src/Components/requests.js
@@ -0,0 +1,15 @@
+const APIKEY = "d4ce302b2659e09aab5cc8a58b391258";
+
+
+const requests = {
+    fetchTreding: `/trending/all/week?api_key=${APIKEY}&language=en-US`,
+    fetchNetFlixOriginals:`/discover/tv?api_key=${APIKEY}&with_networks=213`,
+    fetchTopRated:`/movie/top_rated?api_key=${APIKEY}&language=en-US`,
+    fetchActionMovies:`/discover/movie?api_key=${APIKEY}&with_genres=28`,
+    fetchComedyMovies:`/discover/movie?api_key=${APIKEY}&with_genres=35`,
+    fetchHorroMovies:`/discover/movie?api_key=${APIKEY}&with_genres=27`,
+    fetchRomanticMovies:`/discover/movie?api_key=${APIKEY}&with_genres=10749`,
+    fetchDocumentaries:`/discover/movie?api_key=${APIKEY}&with_genres=99`
+}
+
+export default requests;
